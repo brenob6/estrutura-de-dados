95e370b74f8b204a3bfdec91e8073708967e7c73
brenob6 <brenob6@gmail.com>
2024-01-05 00:12:37 -0300

    stack_test TestSuite

--- test/stack_test.c
+++ test/stack_test.c
@@ -1,9 +1,23 @@
 #include <criterion/criterion.h>
+#include <criterion/internal/test.h>
 #include "../inc/stack.h"
+
+stack *s = NULL;
+
+void setup()
+{
+    s = stack_create(sizeof(int));
+}
+
+void clean()
+{
+    free(s);
+}
+
+TestSuite(stack_test, .init=setup, .fini=clean);
 
 Test(stack_test, create)
 {
-    stack *s = stack_create(sizeof(int)); 
     cr_expect(s != NULL, "");
     cr_expect(s->nelems == 0, "");
     cr_expect(s->top == NULL, "");
